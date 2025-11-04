7bf107df89851dc628972a980f5b8a1b9bb6e7b4
Anitha Natarajan <anataraj@redhat.com>
2025-11-04 06:31:09 +0530

    test

---
+++ markdown/test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Simple HTTP server: responds with a greeting on "/"
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Tekton Webhook!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	addr := ":8080"
+	log.Printf("starting server at %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
+}
